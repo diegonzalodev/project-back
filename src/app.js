9a9b2f9c10b3c6b77ca18c3cd5b46b7c9feadbb0
Diego Gonzalo <diego_rp30@outlook.com>
2023-03-28 21:12:25 -0500

    express installed and endpoints added

---
+++ src/app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { ProductManager } from "./ProductManager.js";
+
+const product = new ProductManager("./products.json");
+const app = express();
+
+app.get(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("App is running");
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const arrayProducts = await product.getProducts();
+    let { limit } = req.query;
+    limit = parseInt(limit);
+
+    if (!limit || limit <= 0 || limit > arrayProducts.length) return res.send(arrayProducts);
+
+    const limitProducts = arrayProducts.slice(0, limit);
+    res.send(limitProducts);
+  } catch (error) {
+    console.log(error);
+    res.send({ error: "Internal server error" });
+  }
+});
+
+app.get("/products/:pid", async (req, res) => {
+  try {
+    const arrayProducts = await product.getProducts();
+    const findByIdProduct = arrayProducts.find((prod) => prod.id === Number(req.params.pid));
+
+    if (!findByIdProduct) return res.send({ error: "This product doesn't exist" });
+
+    res.send(findByIdProduct);
+  } catch (error) {
+    console.log(error);
+    res.send({ error: "Internal server error" });
+  }
+});
+
+app.listen(8080, () => {
+  console.log("Server listen in port 8080");
+});
